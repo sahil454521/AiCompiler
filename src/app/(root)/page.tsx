12d5e889128032d47936cbd831bff6fc21804463
Sahil Pathak <sahilpathak2005@gmail.com>
2025-06-18 22:59:04 +0530

    added the editor panel

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,12 @@
 import Header from "@/app/(root)/_components/Header";
-
+import EditorPanel from "@/app/(root)/_components/EditorPanel";
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-950"> 
       <Header/>
       <div className="max-w-[1800px] mx-auto p-4">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          <EditorPanel/>
         </div>
       </div>
     </div>
