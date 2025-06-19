4824d2d36935b6356e490f544f97e1289769defd
Sahil Pathak <sahilpathak2005@gmail.com>
2025-06-19 10:44:44 +0530

    added the output panel

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/app/(root)/_components/Header";
 import EditorPanel from "@/app/(root)/_components/EditorPanel";
+import OutputPanel from "./_components/OutputPanel";
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-950"> 
@@ -7,6 +8,7 @@ export default function Home() {
       <div className="max-w-[1800px] mx-auto p-4">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
           <EditorPanel/>
+          <OutputPanel/>
         </div>
       </div>
     </div>
